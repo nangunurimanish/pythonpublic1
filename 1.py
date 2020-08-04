94197c906d1d4c58dcfed9807ef35f6dbe651610
nangunurimanish <69221475+nangunurimanish@users.noreply.github.com>
2020-08-04 18:30:50 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+
+"""message = '''hello world\'s 
+manish'''
+print(message)"""
+"""greeting = 'Hello'
+name = ' Manish'
+message =f'{greeting}, {name.upper()}. haha'
+print(message)
+a=59
+b=96
+total=a*b
+print(total)"""
+"""a=10
+num =f'{a+1},hello'
+print(num)
+
+print(5+2)
+print(round(0.95))
+"""
+num1 = '566'
+num2 = '8996'
+num3=int(num1)
+num4=int(num2)
+print(num3+num4)
