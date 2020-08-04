b8f0b18ab150d586f51393b05f2b8940d60f896a
nangunurimanish <69221475+nangunurimanish@users.noreply.github.com>
2020-08-04 18:32:47 -0400

    math funcs

---
+++ 2.py
@@ -0,0 +1,17 @@
+"""number=['manish',2,'3',4,'5',6,]
+num=[11,12,13]
+print(number[1])
+number.remove(6)
+number.pop(2)
+removed=number.pop()
+print(removed)
+print(number)
+nums=[101,256,348,658,452]
+nums.sort(reverse=True)
+print(nums)"""
+"""print('maths' in courses )"""
+import turtle
+my_turtle=turtle.Turtle()
+
+
+turtle.done()
